dce408b9704b9ddb5a5832487f7391d548e96d90
Malaka-MR <malakadarkalfhamd@gmail.com>
2024-10-18 21:11:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,8 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/f1HSsCt/20241013-212427.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://8030.us.kg/file/zOm4HoO6YnQR.jpg",
+AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+AUTO_BIO:"true",
+AUTO_VOICE:"true"
 };
